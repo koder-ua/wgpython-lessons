ce5b31a2d65de931b622c106256162c25d8908b5
DDovgan <sinled@gmail.com>
2014-04-30 15:05:23 +0300

    added exam decorator function

--- 04/forth_iterpreter.py
+++ 04/forth_iterpreter.py
@@ -1,13 +1,61 @@
 # -*- coding:utf8 -*-
 
-def forth(file_name):
+COMMANDS_LIST = (
+    "PUT",
+    "POP",
+    "ADD",
+    "SUB",
+    "PRINT"
+)
+
+
+# parse command
+def parse_command(line, line_number):
+    command_line = line.rstrip().upper().split()
+    command = {}
+    key = ''
+    value = False
+
+    def parse_value(word):
+        try:
+            return int(word)
+        except ValueError:
+            try:
+                return float(word)
+            except ValueError:
+                print "Error in line {0}, wrong value".format(line_number)
+
+
+    if command_line[0] in COMMANDS_LIST:
+        key = command_line[0]
+
+        if len(command_line) > 1:
+            value = parse_value(command_line[1])
+            if value:
+                return {key: value}
+        else:
+            return {key: False}
+    else:
+        print "Error in line {0}, skipping".format(line_number)
+        return False
+
+
+def eval_forth(file_name):
     file = open(file_name, "r")
-
-    data_stack = []
-    commands_stack = ()
-
-    for line in file:
-        print line
+    command_list = []
+    data_stack = ()
 
 
-forth("forth_programm.txt")
+    # parse file
+    for line_number, line in enumerate(file):
+        if line[0] == '#':
+            pass
+        else:
+            command = parse_command(line, line_number)
+            if command:
+                command_list.append(command)
+
+    print command_list
+
+
+eval_forth("forth_programm.txt")
---
+++ exam/decorator.py
@@ -0,0 +1,38 @@
+# -*- coding:utf8 -*-
+# Написать декоратор, реализующий случайную задержку перед вызовом декорируемой функции.
+
+import time
+
+import random
+
+
+min_time = 2
+max_time = 4
+
+
+def wait(max_delay, min_delay):
+    def decorator(fn):
+        def wrapper(*args, **kwargs):
+            delay = min_delay + random.random() * (max_delay - min_delay)
+            time.sleep(delay)
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@wait(max_time, min_time)
+def some_func():
+    pass
+
+
+start_time = time.time()
+some_func()
+duration1 = time.time() - start_time
+
+start_time = time.time()
+some_func()
+duration2 = time.time() - start_time
+
+assert duration1 != duration2
